9520202d8291c5decf95d314c53877464ed4ddc3
codecooker0105 <86731634+codecooker0105@users.noreply.github.com>
2021-11-12 00:47:41 +0800

    Initialize feature/apollo

---
+++ src/routes/home/index.js
@@ -0,0 +1,30 @@
+/**
+ * React Starter Kit (https://www.reactstarterkit.com/)
+ *
+ * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import Home from './Home';
+import newsQuery from './news.graphql';
+import Layout from '../../components/Layout';
+
+async function action({ client }) {
+  const data = await client.query({
+    query: newsQuery,
+  });
+  return {
+    title: 'React Starter Kit',
+    chunks: ['home'],
+    component: (
+      <Layout>
+        <Home news={data.reactjsGetAllNews} />
+      </Layout>
+    ),
+  };
+}
+
+export default action;
